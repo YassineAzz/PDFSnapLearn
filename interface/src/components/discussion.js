109864e570f7b2ce6240a2d0d7099c3fac165390
Yassine Azzimani <yassineazzimani@hotmail.fr>
2024-02-04 17:40:25 +0100

    Chang

---
+++ interface/src/components/discussion.js
@@ -0,0 +1,13 @@
+// DiscussionPage.js
+import React from 'react';
+
+const DiscussionPage = () => {
+  return (
+    <div>
+      <h1>Discussion Page</h1>
+      {/* Le contenu de votre nouvelle page ici */}
+    </div>
+  );
+};
+
+export default DiscussionPage;
